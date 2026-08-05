b111c5001122485dc51708e9701259f4248bb1c1
Fcooperation <thefcooperation@gmail.com>
2026-08-05 20:32:59 +0100

    Refactor CONFIG object to include Supabase URLs

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 window.CONFIG = {
-  API_URL: "https://fweb-backend.onrender.com"
+
+API_URL:
+"https://fweb-backend.onrender.com",
+
+SUPABASE_URL:
+"https://pwsxezhugsxosbwhkdvf.supabase.co",
+
+SUPABASE_ANON_KEY:
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3c3hlemh1Z3N4b3Nid2hrZHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MjgzODcsImV4cCI6MjA2NzUwNDM4N30.T170FX8tC5iZEmdzyY_NjuFQDZ9_7GxxVSrVLzhvnQ0"
+
 };
